9489722d261c06e536895c4a249404581aef59f2
Hasan Masud <hmmasud635@gmail.com>
2025-08-02 03:15:43 +0600

    new added text

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//@@ If the type is comon, Import the results of any function into another page
+//@@ If the type is common js, Import the results of any function into another page
 
 // const result = require("./pageconfig")
 
@@ -10,13 +10,14 @@
 
 // result(20, 30)
 
+//@@ fs modiul
+//@@ file create // file read // file update // text replace // all text delete // file delete // folder create // specific folder in file create ( )
 
-//@@ file create // file read // file update // all text delete // file delete ( )
-const fs = require("fs")
+// const fs = require("fs")
 
 //@@ file create
 //@@ asyncunus
-// fs.writeFile("create.txt", "new file create asyncunus", (error) => {
+// fs.writeFile("create.txt", "new file create", (error) => {
 //   if (error) {
 //     console.log(error)
 //   }
@@ -24,7 +25,7 @@ const fs = require("fs")
 // })
 
 //@@ syncunus
-// const data = fs.writeFileSync("create.txt", "new file create syncunus")
+// const data = fs.writeFileSync("create.txt", "new file create")
 
 // console.log(data);
 
@@ -38,11 +39,23 @@ const fs = require("fs")
 
 
 //@@ file update
-// fs.appendFile("create.txt", "update txt async", (error, data)=>{
+// fs.appendFile("create.txt", "new txt", (error, data)=>{
 //   if(error) {
 //     console.log(error);
 //   }
 //   console.log(data);
+// })
+
+
+//@@ text replace
+// fs.readFile("create.txt", "utf8", (error, data) => {
+//   let textreplace = data.replace("mern2406","update txt")
+//   fs.writeFile("create.txt", textreplace,(err)=> {
+//      if (error) {
+//     console.log(error)
+//   }
+//   console.log(textreplace);
+//   })
 // })
 
 
@@ -66,5 +79,119 @@ const fs = require("fs")
 //     console.log(error);
 //   }
 // })
+
+
+//@@ folder create
+// fs.mkdir("newfolder", (error)=>{
+//   if (error) {
+//     console.log(err);
+//   }
+//   console.log("folder create");
+// })
+
+
+//@@ specific folder in file create
+// fs.writeFile("newfolder/newfile.txt", "new file create", (error) => {
+//   if (error) {
+//     console.log(error)
+//   }
+//   console.log("file created");
+// })
+
+//@@ path modiul (file location)
+//@@ file name // file extention // directory name // file name // path join // path resolve // path parse //
+
+// const path = require("path")
+
+//@@ file name
+// const filename = path.basename("/newfolder/newfile.txt")
+// console.log(filename);
+
+
+//@@ file extention
+// const fileextention = path.extname("/newfolder/newfile.txt")
+// console.log(fileextention);
+
+
+//@@ directory name
+// console.log(__dirname);
+
+//@@ file name
+// console.log(__filename);
+
+//@@ path join
+// const fullpath = path.join("/newfolder", "newfile.txt")
+// console.log(fullpath);
+
+//@@ path join (methote)
+// const fullpath = path.join("/newfolder", "newfile.txt")
+// console.log(fullpath);
+
+//@@ path resolve (problem solve) no test
+// const resolve = path.resolve("/newfolder", "newfile.txt")
+// console.log(resolve);
+
+//@@ path parse
+// const parse = path.parse("/newfolder/newfile.txt")
+// console.log(parse);
+
+
+//@@ os modiul (system location)
+//@@ architecture // memory store // free memory store // cpu // ip address
+
+// const os = require("os")
+
+//@@ architecture
+// console.log(os.arch());
+
+//@@ memory store
+// console.log(os.totalmem()/(1024*1024*1024));
+
+// @@ free memory store
+// console.log(os.freemem()/(1024*1024*1024));
+
+// @@ cpu
+// console.log(os.cpus());
+
+// @@ ip address
+// console.log(os.networkInterfaces());
+
+
+//@@ url modiul (link location)
+//@@ url // url parse
+
+// const url = require("url")
+
+//@@ url
+// let urllink = "https://www.w3schools.com/react/default.asp"
+// console.log(urllink);
+
+//@@ url parse
+// let adr = "https://www.https://www.figma.com/files/team/1392598710001733481/recents-and-sharing?fuid=1392598705731773924.com/react/default.asp"
+// const urlparse = url.parse(adr, true)
+// console.log(urlparse);
+
+
+//@@ http modiul (server)
+//@@ url // url parse
+
+const http = require("http")
+
+//@@ server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {"Content-type": "text/html"})
+res.end("<h1>new file</h1>")
+ })
+
+ server.listen(3000, () => {
+  console.log("server runing....");
+  
+ })
+
+//@@ url parse
+// let adr = "https://www.https://www.figma.com/files/team/1392598710001733481/recents-and-sharing?fuid=1392598705731773924.com/react/default.asp"
+// const urlparse = url.parse(adr, true)
+// console.log(urlparse);
+
 
 
--- pageconfig.js
+++ pageconfig.js
@@ -1,6 +1,6 @@
 
 //একটি কমান্ড করতে হবে।
-// Import the results of any function into another page / যে কোন ফাংশনের রেজাল্টকে অন্য কোন পৃষ্ঠায় ইম্পোর্ট করা। 
+//@@ If the type is common js, Import the results of any function into another page
 
 function total(a, b){
   console.log(a + b);
@@ -9,7 +9,7 @@ function total(a, b){
 
 module.exports = total;
 
-//If the type is module, import the result of any function into another page. Type modulus in the package json. /টাইপ ‍যদি মডিউল হয়, যে কোন ফাংশনের রেজাল্টকে অন্য কোন পৃষ্ঠায় ইম্পোর্ট করা। প্যাকেজ জেসনে টাইপ মডিউলস করা।
+//@ If the type is module, If the type is module, import the result of any function into another page. Type modulus in the package json.
 
 // function total(a, b){
 //   console.log(a + b);
